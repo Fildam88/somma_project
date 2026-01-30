ccc41091dc6e397de770e9c780e65c5d611915bd
Fildam <damianofl1988@gmail.com>
2026-01-30 23:37:41 +0100

    UPDATE CALCOLATORE: Nuove funzioni

--- backend/public/script.js
+++ backend/public/script.js
@@ -2,8 +2,71 @@ document.getElementById("btn").addEventListener("click", async () => {
   const a = Number(document.getElementById("a").value);
   const b = Number(document.getElementById("b").value);
 
-  const r = await fetch(`/api/somma?a=${a}&b=${b}`);
+  const r = await fetch(`/api/Somma?a=${a}&b=${b}`);
   const data = await r.json();
 
   document.getElementById("res").textContent = data.risultato;
 });
+//Sottrazione
+document.getElementById("btn1").addEventListener("click", async () => {
+  const a = Number(document.getElementById("a").value);
+  const b = Number(document.getElementById("b").value);
+
+  const r = await fetch(`/api/Sottrazione?a=${a}&b=${b}`);
+  const data = await r.json();
+
+  document.getElementById("res").textContent = data.risultato;
+});
+//Moltplicazione
+
+document.getElementById("btnMoltiplicazione").addEventListener("click", async () => {
+  const a = Number(document.getElementById("a").value);
+  const b = Number(document.getElementById("b").value);
+
+  const r = await fetch(`/api/Moltiplicazione?a=${a}&b=${b}`);
+  const data = await r.json();
+
+  document.getElementById("res").textContent = data.risultato;
+});
+//Divisione
+
+document.getElementById("btnDivisione").addEventListener("click", async () => {
+  const a = Number(document.getElementById("a").value);
+  const b = Number(document.getElementById("b").value);
+
+  const r = await fetch(`/api/Divisione?a=${a}&b=${b}`);
+  const data = await r.json();
+
+  document.getElementById("res").textContent = data.risultato;
+});
+
+//radice quadra A
+
+document.getElementById("btnRadiceQuadraA").addEventListener("click", async () => {
+  const a = Number(document.getElementById("a").value);
+  
+
+  const r = await fetch(`/api/RadiceQuadraA?a=${a}`);
+  const data = await r.json();
+
+  document.getElementById("res").textContent = data.risultato;
+
+});
+
+
+
+//radice quadra B
+
+document.getElementById("btnRadiceQuadraB").addEventListener("click", async () => {
+  const b = Number(document.getElementById("b").value);
+  
+
+  const r = await fetch(`/api/RadiceQuadraA?a=${b}`);
+  const data = await r.json();
+
+  document.getElementById("res").textContent = data.risultato;
+
+});
+
+
+
--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // API: POST /sum  { "a": 2, "b": 3 }  -> { "result": 5 }
 
-app.get("/api/somma", (req, res) => {
+app.get("/api/Somma", (req, res) => {
   const a = Number(req.query.a);
   const b = Number(req.query.b);
 
@@ -19,6 +19,76 @@ app.get("/api/somma", (req, res) => {
 
   res.json({ risultato: a + b });
 });
+// API: POST /diffrz  { "a": 2, "b": 3 }  -> { "result": -1 }
+app.get("/api/Sottrazione", (req, res) => {
+  const a = Number(req.query.a);
+  const b = Number(req.query.b);
+
+  if (!Number.isFinite(a) || !Number.isFinite(b)) {
+    return res.status(400).json({ errore: "a e b devono essere numeri" });
+  }
+
+  res.json({ risultato: a - b });
+});
+ // API: POST /multpl  { "a": 2, "b": 3 }  -> { "result": 6 }
+app.get("/api/Moltiplicazione", (req, res) => {
+  const a = Number(req.query.a);
+  const b = Number(req.query.b);
+
+  if (!Number.isFinite(a) || !Number.isFinite(b)) {
+    return res.status(400).json({ errore: "a e b devono essere numeri" });
+  }
+
+  res.json({ risultato: a * b });
+});
+// API: POST /divsn  { "a": 2, "b": 3 }  -> { "result": 2/3 }
+
+app.get("/api/Divisione", (req, res) => {
+  const a = Number(req.query.a);
+  const b = Number(req.query.b);
+
+  if (!Number.isFinite(a) || !Number.isFinite(b)) {
+    return res.status(400).json({ errore: "a e b devono essere numeri" });
+  }
+
+  res.json({ risultato: a / b });
+});
+
+
+// API: POST /radice quadra A  { "a":4 }  -> { "result": 2 }
+
+app.get("/api/RadiceQuadraA", (req, res) => {
+  const a = Number(req.query.a);
+  
+
+  if (!Number.isFinite(a)) {
+    return res.status(400).json({ errore: "a e b devono essere numeri" });
+  }
+
+  res.json({ risultato: Math.sqrt(a) });
+});
+
+
+
+// API: POST /radice quadra B  { "a":4 }  -> { "result": 2 }
+
+app.get("/api/RadiceQuadraA", (req, res) => {
+  const b = Number(req.query.b);
+  
+
+  if (!Number.isFinite(b)) {
+    return res.status(400).json({ errore: "a e b devono essere numeri" });
+  }
+
+  res.json({ risultato: Math.sqrt(b) });
+});
+
+
+
+
+
+
+
 
 
 // Home page (opzionale, ma comodo)
